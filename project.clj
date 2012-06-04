f88a9ce0abdde62c5d8f726cbc55aef4385e4305
Emanuel Evans <emanuel.evans@gmail.com>
2012-06-04 08:26:42 -0600

    Initial commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject emanuelevansdotcom "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [hiccup "1.0.0"]
+                 [clodown "1.0.2"]
+                 [clj-time "0.4.2"]])
