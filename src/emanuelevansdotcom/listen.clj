44d6e303bcf69334ef15e0ec0dc7129315e33f38
Emanuel Evans <emanuel.evans@gmail.com>
2012-07-30 18:57:56 -0600

    Add caption option for listen page

--- src/emanuelevansdotcom/listen.clj
+++ src/emanuelevansdotcom/listen.clj
@@ -3,15 +3,21 @@
             (clojure.java [io :as io])
             (hiccup       [element :refer [link-to]])))
 
-(def audio-re #"\d+_(.+).aiff")
+(def audio-re #"(\d+)_(.+).aiff")
+
+(def captions
+  {"2" (link-to "http://www.allegrachapman.com"
+                "Allegra Chapman, piano")})
 
 (defn format-audio [fname]
-  (let [[_ t] (re-find audio-re fname)
+  (let [[_ audio-number t] (re-find audio-re fname)
         audio-title (s/replace t #"_" " ")
         audio-path (str "audio/" fname)]
     [:p
      [:strong audio-title]
      [:br]
+     (if-let [caption (captions audio-number)]
+       (list [:span.caption caption] [:br]))
      [:audio {:controls "controls"}
       [:source {:src (s/replace audio-path #"aiff" "mp3")
                 :type "audio/mpeg"}]
